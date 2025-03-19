4c32b492468234ff56a7308690c39d454340ad06
AnastasiaG24 <gandacov.anastasia@gmail.com>
2025-03-19 23:15:03 +0200

    Am adaugat namespace-ul potrivit

--- eUseControl.Domain/Entities/User/Global/LevelStatus.cs
+++ eUseControl.Domain/Entities/User/Global/LevelStatus.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using eUseControl.Domain.Enums;
 
 namespace eUseControl.Domain.Entities.User.Global
 {
