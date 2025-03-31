1471ae9cbdbc74f1280578e12e6067763c85ca57
ag234ag <andreeagorun2304@gmail.com>
2025-03-31 20:59:22 +0300

    adaugare functii pentru baza de date

--- eUseControl.BusinessLogic/Core/UserApi.cs
+++ eUseControl.BusinessLogic/Core/UserApi.cs
@@ -4,24 +4,36 @@ using eUseControl.Domain.Entities.User;
 using eUseControl.Domain.Entities.Res;
 using eUseControl.Domain.Entities.User.Global;
 using eUseControl.Domain.Entities.Product;
+using eUseControl.BusinessLogic.DBModel;
+using System.Linq;
+using System.Collections.Generic;
+
 
 namespace eUseControl.BusinessLogic.Core
 {
     public class UserApi
     {
-        public ActionStatus UserLogData(ULoginData data)
+        public bool UserSessionStatus()
         {
-            return new ActionStatus();
+            return true;
         }
-
-        internal LevelStatus CheckLevelLogic(string keySession)
+        internal ULoginResp UserSessionData(ULoginData data)
         {
-            return new LevelStatus();
-        }
-
-        internal ProductDetail GetProdUser(int id)
-        {
-            return new ProductDetail();
+            UDbTable result;
+            using (var db = new UserContext())
+            {
+                result = db.Users.FirstOrDefault(u => u.Username == data.Username && u.Password ==
+                data.Password);
+            }
+            if (result == null)
+            {
+                return new ULoginResp
+                {
+                    Status = false,
+                    StatusMsg = "The username or password is incorrect"
+                };
+            }
+            return new ULoginResp { Status = true };
         }
     }
 }
