54fc7682b3c5ac429c3d87a81eb651f3135a2182
ag234ag <andreeagorun2304@gmail.com>
2025-05-20 19:55:27 +0300

    reducerea numărului de controllere prin reorganizare

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -9,75 +9,14 @@ namespace MainApp.Web
           {
                routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-               routes.MapRoute(
-                   name: "Default",
-                   url: "{controller}/{action}/{id}",
-                   defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
 
-               );
-
-               routes.MapRoute(
-                   name: "About",
-                   url: "{controller}/{action}/{id}",
-                   defaults: new { controller = "About", action = "Index_About", id = UrlParameter.Optional }
-
-               );
-               routes.MapRoute(
-                   name: "Contact",
-                   url: "{controller}/{action}/{id}",
-                   defaults: new { controller = "Contact", action = "Index", id = UrlParameter.Optional }
-               );
-               routes.MapRoute(
-                  name: "Services",
-                  url: "Services/{action}/{id}",
-                  defaults: new { controller = "Servicii", action = "Index", id = UrlParameter.Optional }
-              );
-               routes.MapRoute(
-                    name: "Promotii",
-                    url: "{controller}/{action}/{id}",
-                    defaults: new { controller = "Promotii", action = "Index_Promotii", id = UrlParameter.Optional }
-                );
-                routes.MapRoute(
-                    name: "Blog",
-                    url: "{controller}/{action}/{id}",
-                    defaults: new { controller = "Blog", action = "Index", id = UrlParameter.Optional }
-                );
-               routes.MapRoute(
-                    name: "Destinatie",
-                    url: "{controller}/{action}/{id}",
-                    defaults: new { controller = "Pagini", action = "Destinatie", id = UrlParameter.Optional }
-                );
-               routes.MapRoute(
-                    name: "Explorare",
-                    url: "{controller}/{action}/{id}",
-                    defaults: new { controller = "Pagini", action = "Explorare", id = UrlParameter.Optional }
-                );
-                routes.MapRoute(
-                    name: "Galerie",
-                    url: "{controller}/{action}/{id}",
-                    defaults: new { controller = "Pagini", action = "Galerie", id = UrlParameter.Optional }
-                );
-                routes.MapRoute(
-                   name: "Ghid",
-                   url: "{controller}/{action}/{id}",
-                   defaults: new { controller = "Pagini", action = "Ghid", id = UrlParameter.Optional }
-               );
-                routes.MapRoute(
-                   name: "Recenzii",
-                   url: "{controller}/{action}/{id}",
-                   defaults: new { controller = "Pagini", action = "Recenzii", id = UrlParameter.Optional }
-               );
-                routes.MapRoute(
-                  name: "Eroare",
-                  url: "{controller}/{action}/{id}",
-                  defaults: new { controller = "Pagini", action = "Eroare", id = UrlParameter.Optional }
-              );
-               routes.MapRoute(
-                  name: "Rezerveaza_calatorie",
-                  url: "{controller}/{action}/{id}",
-                  defaults: new { controller = "Pagini", action = "Rezerveaza_calatorie", id = UrlParameter.Optional }
-              );
-          }
+            );
+            
+        }
 
      }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,5 +9,45 @@ namespace MainApp.Web.Controllers
           {
                return View();
           }
-     }
+        public ActionResult About()
+        {
+            return View("~/Views/About/Index.cshtml");
+        }
+        public ActionResult Contact()
+        {
+            return View("~/Views/Contact/Index.cshtml");
+        }
+        public ActionResult Services()
+        {
+            return View("~/Views/Services/Index.cshtml");
+        }
+
+        public ActionResult Blog()
+        {
+            return View("~/Views/Blog/Index.cshtml");
+        }
+        public ActionResult Index_Promotii()
+        {
+            return View("~/Views/Promotii/Index_Promotii.cshtml");
+        }
+        public class PromotiiController : Controller
+        {
+            public ActionResult Index()
+            {
+                return RedirectToAction("Index_Promotii", "Home");
+            }
+        }
+
+        public ActionResult Aventura() { return View("~/Views/Blog/Aventura.cshtml"); }
+        public ActionResult Munti() { return View("~/Views/Blog/Munti.cshtml"); }
+        public ActionResult Mare() { return View("~/Views/Blog/Mare.cshtml"); }
+
+        public ActionResult Destinatii() { return View("~/Views/Pagini/Destinatie.cshtml"); }
+        public ActionResult Explorare() { return View("~/Views/Pagini/Explorare.cshtml"); }
+        public ActionResult Galerie() { return View("~/Views/Pagini/Galerie.cshtml"); }
+        public ActionResult Ghid() { return View("~/Views/Pagini/Ghid.cshtml"); }
+        public ActionResult Recenzii() { return View("~/Views/Pagini/Recenzii.cshtml"); }
+        public ActionResult Eroare() { return View("~/Views/Pagini/Eroare.cshtml"); }
+        public ActionResult RezervaCalatorie() { return View("~/Views/Pagini/Rezerveaza_calatorie.cshtml"); }
+    }
 }
